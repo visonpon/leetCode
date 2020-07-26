399593fa89e138ddcebdf69e17909f10d4de1cf1
visonpon <visonpon@gmail.com>
2020-07-26 15:46:25 +0800

    Update LargestNumber.py

--- Array/LargestNumber.py
+++ Array/LargestNumber.py
@@ -81,3 +81,18 @@ class Solution(object):
 
         # print(sorted(map(str, nums), key=makeEqual, reverse=True))
 
+class Solution:
+    def largestNumber(self, nums):
+        n = len(nums)
+        for i in range(n):
+            for j in range(n-i-1):
+                temp_1 = str(nums[j])
+                temp_2 = str(nums[j+1])                
+                if(int(temp_1+temp_2)<int(temp_2+temp_1)):                    
+                    temp = nums[j]                    
+                    nums[j] = nums[j+1]                    
+                    nums[j+1] = temp        
+            output = ''        
+            for i in nums:            
+                output = output + str(i)        
+                return str(int(output))
