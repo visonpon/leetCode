aee19f59a9b8d8cf7cc3a97cabe7b0f0f5181ffe
visonpon <visonpon@gmail.com>
2020-07-26 16:14:18 +0800

    Update MaximumSubarray.py

--- Array/MaximumSubarray.py
+++ Array/MaximumSubarray.py
@@ -53,6 +53,38 @@ class Solution(object):
                 current = current + i
         
         return maxes
+    
+    
+    class Solution:
+      # @param A, a list of integers
+      # @return an integer
+     def maxSubArray(self, A):
+          curr_sum = 0
+          max_sum = -100000
+          for i in range(len(A)):
+              if curr_sum < 0 :
+                  curr_sum = 0
+             curr_sum = curr_sum + A[i]
+             max_sum = max(curr_sum, max_sum)
+         return max_sum
+    
+    
+    
+ public int maxSubArray(int[] A) 
+    {    if(A==null || A.length==0)
+            return 0;    
+            int global = A[0];    
+            int local = A[0];    
+            for(int i=1;i<A.length;i++)    
+                    {
+                          local = Math.max(A[i],local+A[i]);
+                          global = Math.max(local,global);    
+                    }    
+            return global;
+    }
+————————————————
+版权声明：本文为CSDN博主「Code_Ganker」的原创文章，遵循CC 4.0 BY-SA版权协议，转载请附上原文出处链接及本声明。
+原文链接：https://blog.csdn.net/linhuanmars/java/article/details/21314059
 
         # if not nums:
         #     return 0
