b2c215937570bf08c8462c8a2e1907579c811800
visonpon <visonpon@gmail.com>
2020-08-04 08:21:00 +0800

    Update SearchInRotatedSortedArray.py

--- Array/SearchInRotatedSortedArray.py
+++ Array/SearchInRotatedSortedArray.py
@@ -113,3 +113,24 @@ class Solution(object):
         if two != -1:
             return two
         return -1
+      
+      
+      
+class Solution(object):
+    def search(self, nums, target):
+        l , r = 0 ,len(nums)-1 
+        while l <= r :
+            mid  = (l+r) //2
+            if nums[mid] == target:
+                return mid
+            if nums[mid] >= nums[l]: 
+                if target >= nums[l] and target <= nums[mid]:  
+                    r = mid 
+                else : 
+                    l = mid +1
+            if nums[mid] <= nums[r] :
+                if target >= nums[mid] and target <= nums[r]:
+                    l = mid+1
+                else:
+                    r= mid 
+        return -1
